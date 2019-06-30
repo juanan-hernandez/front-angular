50ee2b06dbeb069ddf61dece05ac79a93813c072
juanan-hernandez <juanantoniohb93@gmail.com>
2019-06-30 18:11:32 +0200

    changes in Contact tab

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Injectable } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -11,8 +12,19 @@ import { Component, OnInit, Injectable } from '@angular/core';
 })
 
 export class ContactoComponent implements OnInit {
-
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
   ngOnInit() {
   }
-
+  /**
+   * Process the form we have. Send to whatever backend
+   * Only alerting for now
+   */
+  processForm() {
+    const allInfo = `My name is ${this.name}. The subject is ${this.subject}. My email is ${this.email}. My message is ${this.message}`;
+    alert('hola');
+  }
 }
+
